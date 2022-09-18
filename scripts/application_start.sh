b4df4732cd7ea8be3f3a4a59c386ea95bb61b58d
anubhuti bhardwaj website <workwithanubhuti@gmail.com>
2022-09-18 19:22:10 +0530

    added aws new files

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,10 +8,14 @@ sudo chmod -R 777 /home/ubuntu/Anubhuti-bhardwaj-website
 
 cd /home/ubuntu/Anubhuti-bhardwaj-website
 
-# install node modules
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+#install node modules
 npm install
 
 # start our node app in the background using pm2
 
-sudo pm2 start ‘npm start.’
+node app.js > app.out.log 2> app.err.log < /dev/null &
