8abe20285d0fb50047e1fbcbb0812ca28e2df9dc
anubhuti bhardwaj website <workwithanubhuti@gmail.com>
2022-10-15 16:32:09 +0530

    study material added

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000;
 const blogs = require('./public/scripts/blog');
+const bjmc_courses = require('./public/scripts/bjmc courses');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-  const latestblog = (blogs.sort((a, b) => { return new Date(b.data.date) - new Date(a.data.date);})).slice(0, 3);
-  res.render('index', { latestblog: latestblog})
+  const latestblog = (blogs.sort((a, b) => { return new Date(b.data.date) - new Date(a.data.date); })).slice(0, 3);
+  res.render('index', { latestblog: latestblog })
 })
- 
+
+app.get('/project', (req, res) => {
+  res.render('project')
+})
+
 app.get('/blog', (req, res) => {
-  const allblogs = blogs.sort((a, b) => { return new Date(b.data.date) - new Date(a.data.date);})
+  const allblogs = blogs.sort((a, b) => { return new Date(b.data.date) - new Date(a.data.date); })
   res.render('blog', { allblogs: allblogs })
 })
 
@@ -21,10 +26,21 @@ app.get('/blog/:metatitle', (req, res) => {
   const article = blogs.find(x => x.data.metatitle === (req.params.metatitle));
   var md = require("markdown-it")();
   var result = md.render(article.content);
-  res.render('post', { article: article, post:result });
+  res.render('post', { article: article, post: result });
 })
 
+app.get('/StudyMaterial', (req, res) => {
+  res.render('study material')
+})
+
+app.get('/StudyMaterial/BJMC_Notes', (req, res) => {
+  res.render('bjmc main page')
+})
+
+app.get('/StudyMaterial/BJMC_Notes/BJMC_1st_year_notes', (req, res) => {
+  res.render('bjmc 1st year',{bjmc_courses:bjmc_courses})
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ public/scripts/bjmc courses.js
@@ -0,0 +1,58 @@
+/* 1st year semester 1 */
+
+var course1 = {
+    course_count: "Course-I",
+    course_name: "Introduction to Mass Communication (notes)",
+    course_link: "https://drive.google.com/uc?export=download&id=1QS9Fy1EHvg6Jsz3R9njjZAlkSlQLizhR",
+}
+var course2 = {
+    course_count: "Course-II",
+    course_name: "Computer Basics (notes)",
+    course_link: "https://drive.google.com/uc?export=download&id=129_WQ-ILFd2q8ehBymuJhp4CJTR8fr8e",
+}
+var course3 = {
+    course_count: "Course-III",
+    course_name: "Value Education (notes)",
+    course_link: "https://drive.google.com/uc?export=download&id=1RpQHEdVQcx-NVyqWNzZRAUBDusr9SiBh",
+}
+/*var course4 = {
+    course_count: "Course-IV",
+    course_name: "General Hindi (notes)",
+    course_link: "www.gdrive.com",
+}*/
+
+var semester1 = [course1, course2, course3];
+
+/* 1st year semester 2 
+
+var course1 = {
+    course_count: "Course-VI",
+    course_name: "Introduction to Journalism (notes)",
+    course_link: "www.gdrive.com",
+}
+var course2 = {
+    course_count: "Course-VII",
+    course_name: "Constitution and Indian Political System (notes)",
+    course_link: "www.gdrive.com",
+}
+var course3 = {
+    course_count: "Course-VIII",
+    course_name: "Indian Social System (notes)",
+    course_link: "www.gdrive.com",
+}
+var course4 = {
+    course_count: "Course-IX",
+    course_name: "General English (notes)",
+    course_link: "www.gdrive.com",
+}
+
+var semester2 = [course1, course2, course3, course4];*/
+
+
+/*...............*/
+
+var semester = {
+    semester1,
+}
+
+module.exports = semester;
